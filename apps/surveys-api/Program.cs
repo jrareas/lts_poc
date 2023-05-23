239b94f9b5b6c0388fca73de8974ce103059a3d4
Jose Areas <jose.areas@gov.ab.ca>
2023-05-23 16:00:18 +0000

    Keycloak

--- apps/surveys-api/Program.cs
+++ apps/surveys-api/Program.cs
@@ -4,6 +4,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
+var services = builder.Services;
+services.AddKeycloakAuthentication(authenticationOptions);
+
 app.MapGet("/", () => "Hello World!");
 app.UseRouting();
 // app.MapControllerRoute(name: "hello", pattern: "{controller=Hello}/{action=Index}");
